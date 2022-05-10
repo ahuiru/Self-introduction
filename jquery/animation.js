15979549ad85fd680e142cf8f8d52e0acc1e4d51
ahuiru <zhuangerlangyou@gmail.com>
2022-05-10 16:45:16 +0900

    fifth

--- jquery/animation.js
+++ jquery/animation.js
@@ -1,80 +1,94 @@
-function fadeAnime(){
+$(function() {
+  // ・ナビゲーション
+  $('.openbtn').hide().show(2000);
+  $(".openbtn").click(function () {
+    $(this).toggleClass('active');
+      $("#g-nav").toggleClass('panelactive');
+      $(".circle-bg").toggleClass('circleactive');
+  });
 
-  // ふわっ
-  $('.fadeRightTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-20;//要素より、50px上の
+  $("#g-nav a").click(function () {
+      $(".openbtn").removeClass('active');
+      $("#g-nav").removeClass('panelactive');
+      $(".circle-bg").removeClass('circleactive');
+  });
+
+  // 背景の幾何学
+  particlesJS("particles-js", {"particles":{"number":{"value":57,"density":{"enable":true,"value_area":1122.388442605866}},"color":{"value":"#1f60be"},"shape":{"type":"circle","stroke":{"width":2,"color":"#4169e1"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5371430403899501,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":false,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#4169e1","opacity":0.5371430403899501,"width":0.9620472365193136},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":353.2535873510851,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+
+});
+
+
+// aboutとcreationsとcontactの右からのスライド
+function fadeAnime(){
+  $('.fadeRightTrigger').each(function(){
+    var elemPos = $(this).offset().top-20;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
     if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeRight');// 画面内に入ったらfadeUpというクラス名を追記
-    }else{
-    $(this).removeClass('fadeRight');// 画面外に出たらfadeUpというクラス名を外す
+      $(this).addClass('fadeRight');
+    } else {
+      $(this).removeClass('fadeRight');
     }
-    });
+  });
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  fadeAnime();
+});
 
 
 
 //・最初のローディング
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-  duration: 1000,//時間指定(1000＝1秒)
-  strokeWidth: 0.2,//進捗ゲージの太さ
-  color: '#809aeb',//進捗ゲージのカラー
-  trailWidth: 0.2,//ゲージベースの線の太さ
-  trailColor: '#fff',//ゲージベースの線のカラー
-  text: {//テキストの形状を直接指定
-    style: {//天地中央に配置
+var bar = new ProgressBar.Line(splash_text, {
+  easing: 'easeInOut',
+  duration: 1000,
+  color: '#809aeb',
+  text: {
+    style: {
       position: 'absolute',
       left: '50%',
       top: '50%',
       padding: '0',
-      margin: '-30px 0 0 0',//バーより上に配置
+      margin: '-30px 0 0 0',
       transform:'translate(-50%,-50%)',
       'font-size':'1rem',
       color: '#809aeb',
     },
-    autoStyleContainer: false //自動付与のスタイルを切る
+    autoStyleContainer: false
   },
   step: function(state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+    bar.setText(Math.round(bar.value() * 100) + ' %');
   }
 });
 
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+//アニメーション
+bar.animate(1.0, function () {
+  $("#splash").delay(500).fadeOut(800);
 });
 
 
 
-
-
-//・creationとtechのしたから出てくる動き
+//・creationとtechの下から出てくる動き
 function delayScrollAnime() {
-	var time = 0.2;//遅延時間を増やす秒数の値
+	var time = 0.2;
 	var value = time;
 	$('.delayScroll').each(function () {
-		var parent = this;					//親要素を取得
-		var elemPos = $(this).offset().top;//要素の位置まで来たら
-		var scroll = $(window).scrollTop();//スクロール値を取得
-		var windowHeight = $(window).height();//画面の高さを取得
-		var childs = $(this).children();	//子要素を取得
+		var parent = this;
+		var elemPos = $(this).offset().top;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var childs = $(this).children();
 
-		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//スクロールが入ったら+親要素にクラスplayがなければ
 			$(childs).each(function () {
 
-				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうか
 
 					$(parent).addClass("play");	//親要素にクラス名playを追加
-					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).css("animation-delay", value + "s");
 					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
-					value = value + time;//delay時間を増加させる
+					value = value + time;
 
 					//全ての処理を終わったらplayを外す
 					var index = $(childs).index(this);
@@ -83,18 +97,16 @@ function delayScrollAnime() {
 					}
 				}
 			})
-		}else {
+		} else {
 			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
 			value = time;//delay初期値の数値に戻す
     }
-
 	})
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
-	$(window).scroll(function (){
-		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
-	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  delayScrollAnime();
+});
 
 
 
@@ -115,24 +127,19 @@ modal.click(function() {
 
 
 
-
-
-
 // ・nameのランダム表示
 var Obj = {
 	loop: false,
-	minDisplayTime: 2000,// アニメーションの間隔時間
-	initialDelay: 900, // アニメーション開始までの遅延時間
+	minDisplayTime: 2000,
+	initialDelay: 900,
 	autoStart: true,
 	in: {
-		effect: 'fadeInUp',//animate.css の中にある採用したい動きのクラス名
-		delayScale: 1,// 遅延時間の指数
-		delay: 100,// 文字ごとの遅延時間
-		sync: false,// アニメーションをすべての文字に同時適用するかどうか
-		shuffle: true,// 文字表示がランダムな順に表示されるかどうか
+		effect: 'fadeInUp',
+		delayScale: 1,
+		delay: 100,
+		sync: false,
+		shuffle: true,
 	},
-	out: {// 終了時のアニメーション設定をしたい場合はここに追記
-	}
 }
 var element
 //初期設定
@@ -141,15 +148,9 @@ function RandomInit() {
 	$(element[0]).textillate(Obj);
 }
 
-function RandomAnimeControl() {
-}
-
-// 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
 	RandomInit(); /*初期設定を読み込み*/
-	RandomAnimeControl();/*アニメーション用の関数を呼ぶ*/
-});//ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-
+});
 
 
 
@@ -158,32 +159,4 @@ $(document).ready(function(){
   $("p.question").on("click", function() {
     $(this).next().slideToggle(200);
   });
-});
-
-
-
-
-$(function() {
-  // ・ナビゲーション
-  $('body').hide().fadeIn(1500);
-  $('.openbtn').hide().show(2000);
-  $(".openbtn").click(function () {//ボタンがクリックされたら
-    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-      $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-      $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
-  });
-
-  $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
-      $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-      $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
-      $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
-  });
-
-
-
-
-  // 背景の幾何学
-  particlesJS("particles-js", {"particles":{"number":{"value":57,"density":{"enable":true,"value_area":1122.388442605866}},"color":{"value":"#1f60be"},"shape":{"type":"circle","stroke":{"width":2,"color":"#4169e1"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5371430403899501,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":false,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#4169e1","opacity":0.5371430403899501,"width":0.9620472365193136},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":353.2535873510851,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
-
-
 });
