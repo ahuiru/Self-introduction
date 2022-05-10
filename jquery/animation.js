032e38f2b0397741df3ec537908ebdaba821bcc4
ahuiru <zhuangerlangyou@gmail.com>
2022-05-10 10:47:38 +0900

    third

--- jquery/animation.js
+++ jquery/animation.js
@@ -1,3 +1,22 @@
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeRightTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-20;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeRight');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeRight');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
 
 
 
@@ -27,7 +46,6 @@ var bar = new ProgressBar.Line(splash_text, {//id名を指定
   }
 });
 
-
 //アニメーションスタート
 bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
   $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
@@ -39,7 +57,7 @@ bar.animate(1.0, function () {//バーを描画する割合を指定します 1.
 
 //・creationとtechのしたから出てくる動き
 function delayScrollAnime() {
-	var time = 0.25;//遅延時間を増やす秒数の値
+	var time = 0.2;//遅延時間を増やす秒数の値
 	var value = time;
 	$('.delayScroll').each(function () {
 		var parent = this;					//親要素を取得
@@ -68,7 +86,8 @@ function delayScrollAnime() {
 		}else {
 			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
 			value = time;//delay初期値の数値に戻す
-		}
+    }
+
 	})
 }
 
@@ -76,13 +95,6 @@ function delayScrollAnime() {
 	$(window).scroll(function (){
 		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
 	});// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-// 画面が読み込まれたらすぐに動かしたい場合の記述
-	$(window).on('load', function(){
-		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
-	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
-
-
 
 
 
@@ -113,19 +125,7 @@ function RandomInit() {
 }
 
 function RandomAnimeControl() {
-		var elemPos = $(element[1]).offset().top - 50;
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
-
-		if (scroll >= elemPos - windowHeight) {
-			$(element[1]).textillate(Obj);
-		}
 }
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-	RandomAnimeControl();/*アニメーション用の関数を呼ぶ*/
-});//ここまで画面をスクロールをしたら動かしたい場合の記述
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
@@ -136,56 +136,14 @@ $(window).on('load', function () {
 
 
 
-
-
-
-// 動きのきっかけとなるアニメーションの名前を定義
-// function fadeAnime(){
-
-//   //ふわっと動くきっかけのクラス名と動きのクラス名の設定
-//   $('.fadeInUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
-//   　　var elemPos = $(this).offset().top-50; //要素より、50px上の
-//   　　var scroll = $(window).scrollTop();
-//   　　var windowHeight = $(window).height();
-//   　　if (scroll >= elemPos - windowHeight){
-//   　　$(this).addClass('fadeIn');
-//   　　// 画面内に入ったらfadeInというクラス名を追記
-//   　　}else{
-//   　　　$(this).removeClass('fadeIn');
-//   　　// 画面外に出たらfadeInというクラス名を外す
-//   　　}
-//   　　});
-
-//   //関数でまとめることでこの後に違う動きを追加することが出来ます
-//   $('.fadeInDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
-//   　　var elemPos = $(this).offset().top-50; //要素より、50px上の
-//   　　var scroll = $(window).scrollTop();
-//   　　var windowHeight = $(window).height();
-//   　　if (scroll >= elemPos - windowHeight){
-//   　　$(this).addClass('fadeDown');
-//   　　// 画面内に入ったらfadeDownというクラス名を追記
-//   　　}else{
-//   　　　$(this).removeClass('fadeDown');
-//   　　// 画面外に出たらfadeDownというクラス名を外す
-//   　　}
-//   　　});
-
-
-//   }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
-
-//   // 画面をスクロールをしたら動かしたい場合の記述
-//     $(window).scroll(function (){
-//       fadeAnime();/* アニメーション用の関数を呼ぶ*/
-//     });// ここまで画面をスクロールをしたら動かしたい場合の記述
-
-
-
-
+// question
 $(document).ready(function(){
-  　$("p.question").on("click", function() {
-  　　$(this).next().slideToggle(200);
-  　});
+  $("p.question").on("click", function() {
+    $(this).next().slideToggle(200);
   });
+});
+
+
 
 
 $(function() {
@@ -208,7 +166,7 @@ $(function() {
 
 
   // 背景の幾何学
-  particlesJS("particles-js", {"particles":{"number":{"value":57,"density":{"enable":true,"value_area":1122.388442605866}},"color":{"value":"#1f60be"},"shape":{"type":"circle","stroke":{"width":2,"color":"#4169e1"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5371430403899501,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":false,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#4169e1","opacity":0.5371430403899501,"width":0.9620472365193136},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":353.2535873510851,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+  particlesJS("particles-js", {"particles":{"number":{"value":57,"density":{"enable":true,"value_area":1122.388442605866}},"color":{"value":"#1f60be"},"shape":{"type":"circle","stroke":{"width":2,"color":"#4169e1"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5371430403899501,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":false,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#4169e1","opacity":0.5371430403899501,"width":0.9620472365193136},"move":{"enable":true,"speed":3,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":353.2535873510851,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
 
 });
