528298bc8b568c111a4a87b1a71c9d4d10d39a3b
ahuiru <zhuangerlangyou@gmail.com>
2022-05-10 12:51:22 +0900

    fourth

--- jquery/animation.js
+++ jquery/animation.js
@@ -98,6 +98,23 @@ function delayScrollAnime() {
 
 
 
+// creationsの画像ポップアップ
+const modal = $('.modal-container');
+const img = modal.find('img');
+
+$('img.popup').each(function(index) {
+  $(this).click(function() {
+    img.attr('src', $(this).attr('src'));
+    modal.show();
+  })
+});
+
+modal.click(function() {
+  $(this).hide();
+});
+
+
+
 
 
 
